f3d6ef64321aa51dfd399026f5c2d5be4b7f1e5d
Milos Tomic <milostomic502@gmail.com>
2022-08-07 15:07:16 +0200

    dodao celu rpvu stranu

--- commponents/Header.jsx
+++ commponents/Header.jsx
@@ -1,31 +1,54 @@
 import Link from 'next/link'
-import React from 'react'
-
+import React, { useRef } from 'react'
+import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+import { AiOutlineClose } from 'react-icons/ai'
+import { useStateContext } from '../context/StateContext'
 const Header = () => {
-  return (
-    <div className='heder'>
-        
-        <div className='heder-meni'>
-            <div className='logo'>
-                <p>Inkrist</p>
-            </div>
-            <div className='meni'>
-                <Link href='/onama'>
-                    <p className='meni-text'>O nama</p>
-                </Link>
-                <Link href='/usluge'>
-                    <p className='meni-text'>Usluge</p>
-                </Link>
-                <Link href='/nasi-radovi'>
-                    <p className='meni-text'>Naši radovi</p>
-                </Link>
-                <Link href='/'>
-                    <button type='button'><span>Kontakt</span></button>
-                </Link>
+    const { pokaziMenu, setPokaziMenu } = useStateContext()
+    return (
+        <div className='heder' >
+
+            <div className='heder-meni'>
+                <div className='logo'>
+                    <p>Inkrist</p>
+                </div>
+                <div className='meni'>
+                    <Link href='/onama'>
+                        <p className='meni-text'>O nama</p>
+                    </Link>
+                    <Link href='/usluge'>
+                        <p className='meni-text'>Usluge</p>
+                    </Link>
+                    <Link href='/nasi-radovi'>
+                        <p className='meni-text'>Naši radovi</p>
+                    </Link>
+                    <Link href='/'>
+                        <button type='button' className='kontaktbtn'><span>Kontakt</span></button>
+                    </Link>
+                </div>
+                <div className='menifon'>
+                    {!pokaziMenu ? <p onClick={() => setPokaziMenu(true)}><HiOutlineMenuAlt4 /> </p> : <div className='menuwraper'>
+                        <div className='fonlinkcont'>
+                            <p onClick={() => setPokaziMenu(false)}><AiOutlineClose /></p>
+                            <h3>Menu</h3>
+                            <Link href='/onama'>
+                                <p className='meni-text'>O nama</p>
+                            </Link>
+                            <Link href='/usluge'>
+                                <p className='meni-text'>Usluge</p>
+                            </Link>
+                            <Link href='/nasi-radovi'>
+                                <p className='meni-text'>Naši radovi</p>
+                            </Link>
+                            <Link href='/'>
+                                <button type='button' className='kontaktbtn'><span>Kontakt</span></button>
+                            </Link>
+                        </div>
+                    </div>}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
--- commponents/Lea.jsx
+++ commponents/Lea.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Head from 'next/head';
 import Header from './Header';
 
 const Lea = ({children}) => {
+    const [bgblack, setbgblack] = useState(false)
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                setbgblack(true);
+            } else {
+                setbgblack(false);
+            }
+        });
+    }, [])
   return (
     <div>
         <Head>
             <title>Inkrist</title>
         </Head>
-        <header>
+        <header className={bgblack ? "head bgblack":'head'}>
             <Header />
         </header>
         <div className='main'>
